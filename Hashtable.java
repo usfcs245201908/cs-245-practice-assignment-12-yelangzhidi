c3777a63e1352f2a359fc31ad70a47ba7953f87c
yelangzhidi@gmail.com <T1i2a3n4>
2019-11-27 00:58:16 -0800

    First Implementation

---
+++ Hashtable.java
@@ -0,0 +1,146 @@
+/**
+ * Hash Table with Separate Chaining policy
+ */
+public class Hashtable {
+    HashNode[] bucket;
+    int bucket_size;
+    final double LOAD_THRESHOLD = 0.5;
+    int entries;
+
+    /**
+     * Node of Chains
+     */
+    class HashNode{
+        String key;
+        String value;
+        HashNode next;
+
+        /**
+         * Constructor
+         * @param key the key
+         * @param value the value
+         */
+        HashNode(String key, String value){
+            this.key = key;
+            this.value = value;
+            next = null;
+        }
+    }
+
+    /**
+     * Constructor
+     */
+    Hashtable(){
+        bucket_size = 30000;
+        bucket = new HashNode[bucket_size];
+        entries = 0;
+    }
+    /**
+     * Test if a key/value object pair (with the key matching the argument and any value).
+     * @param key the key
+     * @return {@code true} if there is a key in the HashTable;{@code false} otherwise.
+     */
+    boolean containsKey(String key){
+        return bucket[getHash(key)] != null;
+    }
+
+    /**
+     * Returns the value associated with the key.
+     * @param key the key
+     * @return the value associated with the key which is passed as an argument;
+     * returns ​null​ if no key/value pair is contained by the Hashtable instance.
+     */
+    String get(String key){
+        HashNode head = bucket[getHash(key)];
+        while(head != null){
+            if(head.key.equals(key))
+                return head.value;
+            head = head.next;
+        }
+        return null;
+    }
+
+    /**
+     * Adds the key/value pair into the Hashtable instance.
+     * If there is an existing key/value pair, the Hashtable
+     * instance replaces the stored value with the argument value.
+     * @param key the key
+     * @param value the value
+     */
+    void put(String key, String value) {
+        HashNode head = bucket[getHash(key)];
+        if (head == null)
+            bucket[getHash(key)] = new HashNode(key, value);
+        else {
+            while (head != null) {
+                if (head.key.equals(key)) {
+                    head.value = value;
+                    return;
+                }
+                head = head.next;
+            }
+            HashNode node = new HashNode(key,value);
+            node.next = bucket[getHash(key)];
+            bucket[getHash(key)] =  node;
+        }
+        ++entries;
+        if ((entries * 1.0) / bucket_size >= LOAD_THRESHOLD){
+            increaseBucketSize();
+        }
+    }
+
+    /**
+     * Removes the key/value pair from the Hashtable instance.
+     * @param key the key
+     * @return the value associated with the key to the caller.
+     * @Exception the key is not present in the Hashtable instance.
+     */
+    String remove(String key) throws Exception{
+        HashNode head = bucket[getHash(key)];
+        if(head == null)
+            throw new Exception();
+        if(head.key.equals(key)) {
+            bucket[getHash(key)] = head.next;
+            --entries;
+            return head.value;
+        }
+        else {
+            HashNode prev = head;
+            HashNode curr = null;
+            while(prev.next != null){
+                curr = prev.next;
+                if(curr.key.equals(key)){
+                    prev.next = curr.next;
+                    --entries;
+                    return curr.value;
+                }
+            }
+            return prev.next.value;
+        }
+    }
+
+    /**
+     * Hash key to an index
+     * @param key the key
+     * @return the String to index after hash.
+     */
+    int getHash(String key){
+        return Math.abs(key.hashCode())% bucket_size;
+    }
+
+    /**
+     * Increase the bucket size by times 2.
+     */
+    void increaseBucketSize(){
+        HashNode[] temp = bucket;
+        bucket_size = bucket_size *2;
+        bucket = new HashNode[bucket_size];
+        entries = 0;
+        for(HashNode head : temp) {
+            while(head != null) {
+                put(head.key, head.value);
+                head = head.next;
+            }
+        }
+    }
+}
